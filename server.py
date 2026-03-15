47df79c30c28cc79531cd071589ced79dae397cd
Adam Mogyorosi <adam@fearlessleague.com>
2026-03-15 15:23:10 +0100

    Initial Commit with starter code

--- server.py
+++ server.py
@@ -5,6 +5,11 @@ import typing
 from flask import Flask
 from flask import request
 
+from logger import BattlesnakeDirectLogger
+
+
+LOGGER = BattlesnakeDirectLogger(out_dir="logs")
+
 
 def run_server(handlers: typing.Dict):
     app = Flask("Battlesnake")
@@ -16,17 +21,21 @@ def run_server(handlers: typing.Dict):
     @app.post("/start")
     def on_start():
         game_state = request.get_json()
+        LOGGER.start_game(game_state)
+        LOGGER.log_turn(game_state)
         handlers["start"](game_state)
         return "ok"
 
     @app.post("/move")
     def on_move():
         game_state = request.get_json()
+        LOGGER.log_turn(game_state)
         return handlers["move"](game_state)
 
     @app.post("/end")
     def on_end():
         game_state = request.get_json()
+        LOGGER.end_game(game_state)
         handlers["end"](game_state)
         return "ok"
 
